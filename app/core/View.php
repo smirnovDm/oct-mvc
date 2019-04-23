6acdee304c4d9f46e7df92e1891a0415769ba9b5
dontTochMyNameFuckingBoy <maraffonskiy@gmail.com>
2019-04-23 19:41:24 +0300

    add method render

--- app/core/View.php
+++ app/core/View.php
@@ -1,9 +1,11 @@
 <?php
 
-
 namespace core;
 
-
 class View {
-    //put your code here
+
+    public function render($content_view, $template_view = 'template_main_view') {
+	include_once 'app' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $template_view . '.php';
+    }
+
 }
