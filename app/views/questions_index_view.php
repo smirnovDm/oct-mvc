74768ffc4aa84f0655d2ce151e4b7c1a912d50c0
user <user@user.com>
2019-04-25 20:55:38 +0300

    AJAX

--- app/views/questions_index_view.php
+++ app/views/questions_index_view.php
@@ -11,6 +11,6 @@
     </tbody>
 </table>
 <button id="courses" type="button">Get courses</button>
-<button id="questions-btn" type="button">Get courses</button>
+<button id="questions-btn" type="button">Get questions</button>
 <script src="js/questions.js"></script>
 
--- js/questions.js
+++ js/questions.js
@@ -6,7 +6,7 @@ document.getElementById('courses').onclick = function () {
 	    if (xhr.status === 200) {
 		var json_text = xhr.responseText;
 		var courses = JSON.parse(json_text);
-		for(var i=0; i<courses.lenth; i++){
+		for (var i = 0; i < courses.lenth; i++) {
 		    alert(courses[i].ccy);
 		}
 
@@ -18,27 +18,36 @@ document.getElementById('courses').onclick = function () {
 
 };
 
-function getQuestions(){
-        var xhr = new XMLHttpRequest();
+
+
+document.getElementById('questions-btn').onclick = function () {
+    var xhr = new XMLHttpRequest();
     xhr.open('GET', '/api/questions');
     xhr.onreadystatechange = function () {
 	if (xhr.readyState === 4) {
 	    if (xhr.status === 200) {
 		var json_text = xhr.responseText;
 		var questions = JSON.parse(json_text);
-		return questions;
+		console.log(questions);
+		//TODO вывести в таблицу
+		var tbody = document.querySelector('#questions tbody')
+		tbody.innerHTML = '';
+		for (var i = 0; i < questions.length; i++) {
+		    var tr = '<tr>\n\
+		    <td>' + (i + 1) + '</td>\n\
+		    <td>' + questions[i].name + '</td>\n\
+		    <td>' + questions[i].text + '</td>\n\
+		    </tr>';
+		    tbody.innerHTML += tr;
+		}
 	    } else {
 		return false;
 	    }
 	}
     };
     xhr.send();
-}
 
-document.getElementById('question-btn').onclick = function(){
-    var questions = getQuestions();
-    console.log(questions);
-}
+};
 
 
 
